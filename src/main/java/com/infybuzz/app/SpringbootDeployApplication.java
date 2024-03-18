d46e19be7d03926dff63521dc4be1577de195e09
sailaxman7349 <sailaxman7349@gmail.com>
2024-03-18 23:09:06 +0530

    initial code

---
+++ src/main/java/com/infybuzz/app/SpringbootDeployApplication.java
@@ -0,0 +1,21 @@
+package com.infybuzz.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+
+public class SpringbootDeployApplication {
+
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "Hi babai";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDeployApplication.class, args);
+	}
+
+}
